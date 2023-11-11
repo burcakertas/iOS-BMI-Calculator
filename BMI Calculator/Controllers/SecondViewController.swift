493e13baa7a7444fcc9c5db0746b9c6e4f5f8e62
burcakertas <burcakertas@sabanciuniv.edu>
2023-11-11 15:38:22 +0300

    First commit.

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Burcak Ertas on 5.07.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var yourResultLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value) + "kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let heightValue = heightSlider.value
+        let weightValue = weightSlider.value
+        let calculatedValue = weightValue / pow(heightValue, 2)
+        print(String(format: "%.1f", calculatedValue))
+    }
 }
 
